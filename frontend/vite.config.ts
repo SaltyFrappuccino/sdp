c76d7816b9e5ec996ebb5751e45f082c9d808850
SaltyFrappuccino <saltyfrappuccino@internet.ru>
2025-07-27 00:06:00 +0400

    Update proxy for frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://193.162.143.80:3000',
+        target: 'http://localhost:3000',
         changeOrigin: true,
       },
     },
