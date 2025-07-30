f57eeb5594df0192e68913fcf17c3d5e3a2debe9
SaltyFrappuccino <saltyfrappuccino@internet.ru>
2025-07-30 18:51:29 +0400

    Increase size limit for api

--- backend/src/api.ts
+++ backend/src/api.ts
@@ -21,7 +21,10 @@ const storage = multer.diskStorage({
   }
 });
 
-const upload = multer({ storage });
+const upload = multer({
+  storage,
+  limits: { fileSize: 10 * 1024 * 1024 }, // 10 MB
+});
 
 router.post('/upload', upload.single('image'), (req: Request, res: Response) => {
   if (!(req as any).file) {
